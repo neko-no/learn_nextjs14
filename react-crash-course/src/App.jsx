768b540d0fb3927a2b3e10b5a30a37f709ee11a9
neko-no <101441574+neko-no@users.noreply.github.com>
2024-02-10 13:33:57 +0900

    #3 add: Custom Componet

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import Post from './components/Post'
 
 function App() {
 
   return (
     <>
       <h2>Hello React</h2>
+      <Post />
     </>
   )
 }
---
+++ react-crash-course/src/components/Post.jsx
@@ -0,0 +1,8 @@
+function Post() {
+    return (<div>
+        <p> Maximilian</p>
+        <p>React.js is awesome!</p>
+    </div>)
+}
+
+export default Post;
