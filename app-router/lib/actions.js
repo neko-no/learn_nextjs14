cf69ffd6df433c6f8d5d7b485ec4fd4cfbf88941
neko-no <101441574+neko-no@users.noreply.github.com>
2024-02-17 20:28:42 +0900

    #4 change: フォームで無効な値を入力された場合の処理を行う

--- app-router/lib/actions.js
+++ app-router/lib/actions.js
@@ -3,7 +3,11 @@
 import { saveMeal } from "./meals";
 import { redirect } from "next/navigation";
 
-export async function shareMeal(formData) {
+function isInvalidText(text) {
+    return !text || text.trim() === '';
+}
+
+export async function shareMeal(prevState, formData) {
 
     const meal = {
         title: formData.get('title'),
@@ -12,6 +16,21 @@ export async function shareMeal(formData) {
         image: formData.get('image'),
         creator: formData.get('name'),
         creator_email: formData.get('email')
+    }
+
+    if (
+        isInvalidText(meal.title) ||
+        isInvalidText(meal.summary) ||
+        isInvalidText(meal.instructions) ||
+        isInvalidText(meal.creator) ||
+        isInvalidText(meal.creator_email) ||
+        !meal.creator_email.includs('@') ||
+        !meal.image ||
+        meal.image.size === 0
+    ) {
+        return {
+            message: 'Invalid input.'
+        }
     }
 
     await saveMeal(meal);
